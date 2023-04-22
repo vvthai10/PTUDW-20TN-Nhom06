3a4f7815882e0f2cf4a289b65484bbe9736b3550
QUÍ TỪ VĂN <20120561@student.hcmus.edu.vn>
2023-04-22 11:50:11 +0700

    Add Editor Censorship Interface, fix height for some pages

---
+++ js/editor_censor.js
@@ -0,0 +1,10 @@
+mobiscroll.setOptions({
+    locale: mobiscroll.localeEn,                                         // Specify language like: locale: mobiscroll.localePl or omit setting to use default
+    theme: 'ios',                                                        // Specify theme like: theme: 'ios' or omit setting to use default
+    themeVariant: 'light'                                                // More info about themeVariant: https://docs.mobiscroll.com/5-23-2/javascript/select#opt-themeVariant
+});
+
+mobiscroll.select('#demo-multiple-select', {
+    inputElement: document.getElementById('demo-multiple-select-input')  // More info about inputElement: https://docs.mobiscroll.com/5-23-2/javascript/select#opt-inputElement
+});
+
