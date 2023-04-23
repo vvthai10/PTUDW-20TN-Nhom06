5fd1de0db66b63545f92973f0a81fae7a9b3a586
QUÍ TỪ VĂN <20120561@student.hcmus.edu.vn>
2023-04-23 20:57:57 +0700

    Add some corresponding tables in Admin page

--- js/admin.js
+++ js/admin.js
@@ -7,6 +7,12 @@ $$("li.sidenav__link").forEach((item)=>{
         console.log("is-selected remove")
         item.classList.add("is-selected");
         $(".main h3").innerText = item.firstChild.innerText 
+        
+        $$(".manage-table").forEach((table) => {
+            table.style.display = "none";
+        })
+        console.log('#'+item.id+'-table');
+        $('#'+item.id+'-table').style.display="block";
     })
 });
 
