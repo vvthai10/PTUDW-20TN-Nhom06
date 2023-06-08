f08fdab65b169e642e89502822f915406dd044e0
vvthai10 <vuvanthai1410@gmail.com>
2023-06-08 23:16:04 +0700

    handle forgot password

---
+++ controllers/jwt.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "nhom06";
+
+function sign(email, expiresIn = "30m") {
+  return jwt.sign({ email }, process.env.JWT_SECRET || JWT_SECRET, {
+    expiresIn,
+  });
+}
+
+function verify(token) {
+  try {
+    jwt.verify(token, process.env.JWT_SECRET || JWT_SECRET);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
+module.exports = { sign, verify };
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const controller = require("../controllers/authController");
 const { body, getErrorMessage } = require("../controllers/validator");
 
-router.get("/login", controller.show);
+router.get("/login", controller.showLogin);
 router.post(
   "/login",
   body("email")
@@ -25,6 +25,83 @@ router.post(
   controller.login
 );
 
+router.get("/register", controller.showRegister);
+router.post(
+  "/register",
+  body("name").trim().notEmpty().withMessage("Name is required!"),
+  body("email")
+    .trim()
+    .notEmpty()
+    .withMessage("Email is required!")
+    .isEmail()
+    .withMessage("Invalid Email address"),
+  body("password").trim().notEmpty().withMessage("Password is required!"),
+  body("password")
+    .matches(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/)
+    .withMessage(
+      "Password must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
+    ),
+  body("confirmPassword").custom((confirmPassword, { req }) => {
+    if (confirmPassword != req.body.password) {
+      throw new Error("Password not match!");
+    }
+    return true;
+  }),
+  (req, res, next) => {
+    let message = getErrorMessage(req);
+    if (message) {
+      return res.render("register", { registerMessage: message });
+    }
+    next();
+  },
+  controller.register
+);
+
 router.get("/logout", controller.logout);
+
+// TODO: Quên mật khẩu
+router.get("/forgot", controller.showForgotPassword);
+router.post(
+  "/forgot",
+  body("email")
+    .trim()
+    .notEmpty()
+    .withMessage("Email is required!")
+    .isEmail()
+    .withMessage("Invalid Email address"),
+  (req, res, next) => {
+    let message = getErrorMessage(req);
+    if (message) {
+      return res.render("forgot-password", { message });
+    }
+    next();
+  },
+  controller.forgotPassword
+);
+
+router.get("/reset", controller.showResetPassword);
+router.post(
+  "/reset",
+  body("password").trim().notEmpty().withMessage("Password is required!"),
+  body("password")
+    .matches(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/)
+    .withMessage(
+      "Password must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
+    ),
+  body("confirmPassword").custom((confirmPassword, { req }) => {
+    if (confirmPassword != req.body.password) {
+      throw new Error("Password not match!");
+    }
+    return true;
+  }),
+  (req, res, next) => {
+    let message = getErrorMessage(req);
+    if (message) {
+      return res.render("forgot-password", { message });
+    }
+    next();
+  },
+  controller.resetPassword
+);
 
 module.exports = router;
