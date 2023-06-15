388ee84fd0c9a1be46174008520d53f4e926fc5e
vvthai10 <vuvanthai1410@gmail.com>
2023-06-15 23:51:41 +0700

    handle show account view v1

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -9,7 +9,21 @@ const authController = require("../controllers/authController");
 router.use(authController.isLoggedIn);
 
 router.get("/account", (req, res) => {
-  res.render("account", { layout: "layout_simple.hbs" });
+  let view = req.query.view;
+  console.log(res.locals.userInfo);
+  if (view == "info") {
+    res.locals.info = 1;
+    res.render("account-info");
+  } else if (view == "password") {
+    res.locals.password = 1;
+    res.render("account-password");
+  } else if (view == "activities") {
+    res.locals.activities = 1;
+    res.render("account-activities");
+  } else if (view == "payment") {
+    res.locals.payment = 1;
+    res.render("account-payment");
+  }
 });
 
 module.exports = router;
